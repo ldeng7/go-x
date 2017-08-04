b4a92007f60b2cf789711b1432aec84a19e4c54a
Lynn Deng <beednln@163.com>
2017-08-04 23:23:29 +0800

    add assert

---
+++ assert/assert.go
@@ -0,0 +1,65 @@
+package assert
+
+import (
+	"reflect"
+	"runtime"
+	"testing"
+
+	"github.com/kr/pretty"
+)
+
+type Assert struct {
+	t *testing.T
+}
+
+func New(t *testing.T) *Assert {
+	return &Assert{t}
+}
+
+func (a *Assert) assert(b bool, fn func(), cd int) {
+	if b {
+		return
+	}
+	_, file, line, _ := runtime.Caller(cd + 1)
+	a.t.Errorf("%s:%d", file, line)
+	if nil != fn {
+		fn()
+	}
+	a.t.FailNow()
+}
+
+func (a *Assert) Equal(got, exp interface{}) {
+	fn := func() {
+		for _, desc := range pretty.Diff(got, exp) {
+			a.t.Error("!", desc)
+		}
+	}
+	a.assert(reflect.DeepEqual(got, exp), fn, 1)
+}
+
+func (a *Assert) NotEqual(got, exp interface{}) {
+	a.assert(!reflect.DeepEqual(got, exp), nil, 1)
+}
+
+func (a *Assert) True(b bool) {
+	a.assert(b, nil, 1)
+}
+
+func (a *Assert) False(b bool) {
+	a.assert(!b, nil, 1)
+}
+
+func (a *Assert) Nil(got interface{}) {
+	a.assert(nil == got, nil, 1)
+}
+
+func (a *Assert) NotNil(got interface{}) {
+	a.assert(nil != got, nil, 1)
+}
+
+func (a *Assert) Fail(desc string) {
+	fn := func() {
+		a.t.Error("!", desc)
+	}
+	a.assert(false, fn, 1)
+}
---
+++ assert/assert_test.go
@@ -0,0 +1,32 @@
+package assert
+
+import (
+	"testing"
+)
+
+type ss struct {
+	i int
+	t tt
+}
+
+type tt struct {
+	i int
+}
+
+func Test(t *testing.T) {
+	ast := New(t)
+	ast.True(true)
+	ast.False(false)
+	ast.Equal(1, 1)
+	i1, i2 := 1, 1
+	ast.Equal(&i1, &i2)
+	ast.Equal("ab", "a"+"b")
+	ast.Equal([]int{1, 2}, []int{1, 2})
+	ast.Equal(map[string]int{"a": 1, "b": 2}, map[string]int{"b": 2, "a": 1})
+	ast.Equal(tt{i: 1}, tt{i: 1})
+	ast.Equal([]tt{tt{i: 1}}, []tt{tt{i: 1}})
+	ast.Equal(ss{i: 1, t: tt{i: 2}}, ss{i: 1, t: tt{i: 2}})
+	ast.NotEqual(1, 0)
+	ast.Nil(nil)
+	ast.NotNil(0)
+}
